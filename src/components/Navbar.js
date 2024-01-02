4f04a85be07f27561ad69de10f4e53e7508e0095
Deepak kumar <deepak21062000@gmail.com>
2024-01-02 12:49:06 +0530

    1st changes

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Link from "next/link";
+import Logo from "./Logo";
+
+const Navbar = () => {
+  return <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
+    <nav>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/projects">Projects</Link>
+        <Link href="/articles">Articles</Link>
+    </nav>
+   
+    <nav>
+        <Link href="/" target={"_blank"}>T</Link>
+        <Link href="/" target={"_blank"}>T</Link>
+        <Link href="/" target={"_blank"}>T</Link>
+        <Link href="/" target={"_blank"}>T</Link>
+        <Link href="/" target={"_blank"}>T</Link>
+        <Link href="/" target={"_blank"}>T</Link>
+    </nav>
+    <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+    <Logo/>
+    </div>
+  </header>;
+};
+
+export default Navbar;
